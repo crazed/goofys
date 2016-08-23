233c0018ebf6610fd6bc2c7d7c3f0c1f9e4dbfed
Allan Feid <feid@hioscar.com>
2016-08-23 12:37:21 -0400

    Create a new mount file

---
+++ goofys/mount.go
@@ -0,0 +1,40 @@
+package goofys
+
+import (
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/jacobsa/fuse"
+	"github.com/jacobsa/fuse/fuseutil"
+)
+
+func Mount(bucket, mountPoint string) (mfs *fuse.MountedFileSystem, err error) {
+	awsConfig := &aws.Config{
+		Region: aws.String("us-east-1"),
+	}
+	awsConfig.S3ForcePathStyle = aws.Bool(true)
+	dur, _ := time.ParseDuration("1m")
+	flags := &FlagStorage{
+		MountOptions: make(map[string]string),
+		DirMode:      os.FileMode(600),
+		FileMode:     os.FileMode(600),
+		Uid:          uint32(99),
+		Gid:          uint32(99),
+		StatCacheTTL: dur,
+		TypeCacheTTL: dur,
+		DebugFuse:    true,
+		DebugS3:      true,
+		Foreground:   false,
+	}
+
+	fs := NewGoofys(bucket, awsConfig, flags)
+	server := fuseutil.NewFileSystemServer(fs)
+	mountCfg := &fuse.MountConfig{
+		FSName:                  bucket,
+		Options:                 flags.MountOptions,
+		DisableWritebackCaching: true,
+	}
+	mfs, err = fuse.Mount(mountPoint, server, mountCfg)
+	return
+}
